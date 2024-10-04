5817fe79f9ca6494e094699fb48764bebb6139e6
Jotaoesee <jadiazm1995@gmail.com>
2024-10-04 20:01:58 +0200

    añado navegacion fragmento registro a fragmento login si el usuario quiere volver

--- app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragments/LoginFragment.kt
+++ app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragments/LoginFragment.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import org.eurekamps.dam2_2425_actividad1.R
 
 
 
 class LoginFragment : Fragment() {
+
+    lateinit var txEmail: EditText
+    lateinit var txContraseña: EditText
+    lateinit var txInicioSesion: TextView
+    lateinit var btnLogin: Button
+    lateinit var btnRegistrar: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +35,27 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-            }
+        // Inicializar las vistas aquí, usando el parámetro 'view' proporcionado por onViewCreated
+        txEmail = view.findViewById(R.id.txEmail)
+        txContraseña = view.findViewById(R.id.txContraseña)
+        txInicioSesion = view.findViewById(R.id.txInicio)
+        btnLogin = view.findViewById(R.id.btnLogin)
+        btnRegistrar = view.findViewById(R.id.btnRegistrar)
+
+        // Aquí añado listeners o cualquier lógica de manejo de eventos
+        btnLogin.setOnClickListener {
+
+        }
+
+        btnRegistrar.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registroFragment)
+        }
+    }
+
+    companion object {
+
     }
 }
--- app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragments/RegistroFragment.kt
+++ app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragments/RegistroFragment.kt
@@ -5,12 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import org.eurekamps.dam2_2425_actividad1.R
 
 
 
 
 class RegistroFragment : Fragment() {
+
+    lateinit var txEmail: EditText
+    lateinit var txContraseña: EditText
+    lateinit var txConfirmarContraseña: EditText
+    lateinit var btnVolver: Button
+    lateinit var btnRegistrar: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,12 +36,27 @@ class RegistroFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_registro, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        // Inicializar las vistas aquí
+        txEmail = view.findViewById(R.id.txEmail)
+        txContraseña = view.findViewById(R.id.txContraseña)
+        txConfirmarContraseña = view.findViewById(R.id.txConfirmarContraseña)
+        btnVolver = view.findViewById(R.id.btnVolver)
+        btnRegistrar = view.findViewById(R.id.btnRegistrar)
+
+        // Aquí añado listeners o cualquier lógica de manejo de eventos
+        btnRegistrar.setOnClickListener {
+
+        }
+
+        btnVolver.setOnClickListener {
+            findNavController().navigate(R.id.action_registroFragment_to_loginFragment)
+        }
+    }
+
     companion object {
 
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegistroFragment().apply {
 
-            }
     }
 }
