d942298747815a1e20b6c3eb779dfdc4c83a115c
Jotaoesee <jadiazm1995@gmail.com>
2024-10-12 13:12:04 +0200

    añado que en el splashFragment compruebe si el usuario esta registrado, si lo esta se va al perfilFragment y si no al loginFragment

---
+++ app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragments/SplashFragment.kt
@@ -0,0 +1,44 @@
+package org.eurekamps.dam2_2425_actividad1.fragments
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import org.eurekamps.dam2_2425_actividad1.R
+
+class SplashFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Inicializar Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        // Usar un Handler para retrasar la ejecución y mostrar el SplashScreen por unos segundos
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Verificar si hay un usuario autenticado
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                // Usuario autenticado, navegar al PerfilFragment
+                findNavController().navigate(R.id.action_splashFragment_to_perfilFragment)
+            } else {
+                // No hay usuario autenticado, navegar al LoginFragment
+                findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+            }
+        }, 2000) // Espera de 2 segundos antes de redirigir
+    }
+}
