7d5580d4a20944178020cbbaa654858c8d5975c7
Jotaoesee <jadiazm1995@gmail.com>
2024-10-25 21:24:43 +0200

    ultimo commit ya que lo dejo por hoy

--- app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragmentsHome/SeleccionProfileFragment.kt
+++ app/src/main/java/org/eurekamps/dam2_2425_actividad1/fragmentsHome/SeleccionProfileFragment.kt
@@ -16,6 +16,12 @@ import org.eurekamps.dam2_2425_actividad1.viewmodel.ProfileViewModel
 
 class SeleccionProfileFragment : Fragment() {
 
+    lateinit var nombreUsuario: TextView
+    lateinit var apellidosUsuario: TextView
+    lateinit var hobbiesUsuario: TextView
+    lateinit var imagenSeleccionada: ImageView
+    lateinit var editarFoto: Button
+
     // Obtener la instancia del ViewModel
     private val profileViewModel: ProfileViewModel by viewModels()
 
@@ -33,22 +39,30 @@ class SeleccionProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        nombreUsuario = view.findViewById(R.id.nombreUsuario)
+        apellidosUsuario = view.findViewById(R.id.apellidosUsuario)
+        hobbiesUsuario = view.findViewById(R.id.hobbiesUsuario)
+        imagenSeleccionada = view.findViewById(R.id.imagenSeleccionada)
+        editarFoto = view.findViewById(R.id.editarFoto)
+
         // Observar cambios en la URL de la imagen del perfil
         profileViewModel.profileImageUrl.observe(viewLifecycleOwner) { url ->
             if (url != null) {
                 // Asignar la imagen de perfil
-                val imgProfileSelected: ImageView = view.findViewById(R.id.ivProfileSelected)
+                val imgProfileSelected: ImageView = view.findViewById(R.id.imagenSeleccionada)
                 Picasso.get().load(url).into(imgProfileSelected)
             }
         }
 
         // Asignar los textos a los TextViews (debes manejar los demás campos en el ViewModel también si es necesario)
-        val profileName = "Nombre del perfil" // Obtén el nombre del perfil según sea necesario
-        view.findViewById<TextView>(R.id.list_cell_txtview_name).text = "Nombre: $profileName"
+        val perfilNombre = "Nombre del perfil" // Obtén el nombre del perfil según sea necesario
+        val perfilApellidos = "Apellidos del perfil" // Obtén los apellidos del perfil según sea necesario
+        val perfilHobbies = "Hobbies del perfil" // Obtén los hobbies del perfil según sea necesario
 
         // Asignar listener al botón "Edit"
-        view.findViewById<Button>(R.id.button8).setOnClickListener {
-            findNavController().navigate(R.id.action_homeSelectProfFragment_to_PhotoProfileFragment)
+        view.findViewById<Button>(R.id.editarFoto).setOnClickListener {
+            findNavController().navigate(R.id.action_seleccionProfileFragment_to_fotoPerfilFragment)
         }
     }
 }
